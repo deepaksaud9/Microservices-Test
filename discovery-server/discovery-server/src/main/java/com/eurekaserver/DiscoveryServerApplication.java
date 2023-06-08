cc2221e4cca4d4aad7584631d8fb3586d8be998e
deepak saud <deppofficial00@gmail.com>
2023-06-08 21:23:41 +0545

    rest templete declared

--- discovery-server/discovery-server/src/main/java/com/eurekaserver/DiscoveryServerApplication.java
+++ discovery-server/discovery-server/src/main/java/com/eurekaserver/DiscoveryServerApplication.java
@@ -3,13 +3,21 @@ package com.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate getRestTemplete(){
+		return getRestTemplete();
 	}
 
 }
